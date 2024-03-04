7393a9cf7bdd9264e79a267ead65d383874fa8d8
Pétala Leite <petalaleite@gmail.com>
2024-03-04 17:23:18 -0300

    add gitignore

--- WildlifeAnimalManagement/Program.cs
+++ WildlifeAnimalManagement/Program.cs
@@ -3,6 +3,36 @@ string animalID = "";
 string animalAge = "";
 string animalName = "";
 string animalRescureLocation = "";
-string animalEntryDate = "";
+string animalAdmissionDate = "";
 
 int maxAnimals = 10;
+string selectedOption = "";
+string? readInput = "";
+
+do
+{
+  Console.WriteLine("Welcome to Name Wildlife Conservation Program. What would you like to do?\n");
+  Console.WriteLine("\t1. List all current animals information");
+  Console.WriteLine("\t2. Add new animal.");
+  Console.WriteLine();
+  Console.WriteLine("Enter you selection number (or type Exit to exit the program)");
+
+  readInput = Console.ReadLine();
+  if (readInput != null) selectedOption = readInput.ToLower();
+
+  switch (selectedOption)
+  {
+    case "1":
+      Console.WriteLine("Coming soon...");
+      Console.WriteLine("Press enter to continue");
+      readInput = Console.ReadLine();
+      break;
+    case "2":
+      Console.WriteLine("Coming soon...");
+      Console.WriteLine("Press enter to continue");
+      readInput = Console.ReadLine();
+      break;
+    default:
+      break;
+  }
+} while (selectedOption != "exit");
--- WildlifeAnimalManagement/obj/Debug/net8.0/WildlifeAnimalManagement.AssemblyInfo.cs
+++ WildlifeAnimalManagement/obj/Debug/net8.0/WildlifeAnimalManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WildlifeAnimalManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06285f8d221f5eb1962f9512fcaa7a0e3fc4a7fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WildlifeAnimalManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WildlifeAnimalManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
